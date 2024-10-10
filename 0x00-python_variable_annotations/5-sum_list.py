bab2b17e246997e1c9f06742523e9d6615e438d6
a3ela <abelsintayehu626@gmail.com>
2024-10-10 18:22:34 +0300

    comment added

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-
+'''module for sum_list'''
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
+    '''returns the sum of the list of floats'''
     return sum(input_list)
