2b4678105f305c4dea7a3befec509d41928b4a01
a3ela <abelsintayehu626@gmail.com>
2024-10-10 23:03:17 +0300

    added comment

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python3
-'''module for safe_first_element'''
-from typing import Any, Sequence, Union
+from typing import Sequence, Any, Union
+'''module that takes a sequence and returns its first element
+'''
 
 
-# The types of the elements of the input are not know
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    '''returns the first element of a list if it exists'''
+    '''function that takes a sequence and returns its first element
+    '''
     if lst:
         return lst[0]
     else:
