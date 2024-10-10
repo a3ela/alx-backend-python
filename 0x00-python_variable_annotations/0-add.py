e859b714f57678698d0d755283149a6f54d1c19c
a3ela <abelsintayehu626@gmail.com>
2024-10-10 17:51:55 +0300

    added main

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 '''function to add two float numbers.
 '''
-def add(a: float, b: float) -> float
+def add(a: float, b: float) -> float:
     '''add two floating point numbers.
-    ''':
+    '''
     return a + b
 
