84c467ea3fb3842ee9b4b08a9c528c180a688543
a3ela <abelsintayehu626@gmail.com>
2024-10-10 22:45:46 +0300

    fixed the return type

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 '''module for element_length'''
-from typing import Sequence, List, Iterable
+from typing import Sequence, List, Iterable, Tuple
 
 
-def element_length(lst: Iterable[Squence[int]]) -> List[int]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     '''returns a list of tuples containing elements and their lengths'''
     return [(i, len(i)) for i in lst]
